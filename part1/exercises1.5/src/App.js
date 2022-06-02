44214e376f465b4eedbd17fa58d9b7300ddff3d2
linda8850 <lijia9988@qq.com>
2022-06-02 20:56:32 +0800

    part1->exercises1.5 have done!

---
+++ part1/exercises1.5/src/App.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.part} {props.exer}
+      </p>
+    </>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part[0].name} exer={props.part[0].exercises} />
+      <Part part={props.part[1].name} exer={props.part[1].exercises} />
+      <Part part={props.part[2].name} exer={props.part[2].exercises} />
+    </div>
+  );
+};
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Number of exercises{' '}
+        {props.exer[0].exercises +
+          props.exer[1].exercises +
+          props.exer[2].exercises}
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content part={course.parts} />
+      <Total exer={course.parts} />
+    </div>
+  );
+};
+
+export default App;
